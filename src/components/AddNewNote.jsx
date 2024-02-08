56311203736eb2a2eae2fb8739a014d8ba1cba7e
easmmava <easmmava@gmail.com>
2024-02-08 23:46:34 +0330

    commit 3

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,11 +1,17 @@
 function AddNewNote() {
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="add-new-note">
       <h2>Add New Note</h2>
-      <form className="note-form">
-        <input type="text" className="text-field" />
-        <input type="text" className="text-field" />
-        <button className="btn btn--primary">Add New Note</button>
+      <form className="note-form" onSubmit={HandleSubmit}>
+        <input type="text" className="text-field" placeholder="note title"/>
+        <input type="text" className="text-field" placeholder="note descriotion"/>
+        <button className="btn btn--primary" >
+          Add New Note
+        </button>
       </form>
     </div>
   );
