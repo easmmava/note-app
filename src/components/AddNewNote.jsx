55e9ef6c2c020948f9f1e0a154bc02e9e82940a0
easmmava <easmmava@gmail.com>
2024-02-09 01:52:03 +0330

    commit 3

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
+
 function AddNewNote() {
-  const HandleSubmit = (e) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
     e.preventDefault();
+
+    const newNote = {
+      title,
+      description,
+      id: Date.now(),
+      completed: false,
+      createdAt: new Date().toISOString(),
+    };
+    title("");
+    description("");
   };
 
   return (
     <div className="add-new-note">
       <h2>Add New Note</h2>
-      <form className="note-form" onSubmit={HandleSubmit}>
-        <input type="text" className="text-field" placeholder="note title"/>
-        <input type="text" className="text-field" placeholder="note descriotion"/>
-        <button className="btn btn--primary" >
-          Add New Note
-        </button>
+      <form className="note-form" onSubmit={handleSubmit}>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="note title"
+        />
+        <input
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="note descriotion"
+        />
+        <button className="btn btn--primary">Add New Note</button>
       </form>
     </div>
   );
